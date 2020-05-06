001d16bb17645521c274be7f39b713a78d7023e3
aparajitverma <52506566+aparajitverma@users.noreply.github.com>
2020-05-06 20:57:12 +0530

    Add files via upload

---
+++ Polynomial regression/poly reg.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 17 15:16:51 2019
+
+@author: HYDRA
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+m=100
+X=6*np.random.randn(m,1) - 3
+y=0.5*X**2+ X + 2 +np.random.randn(m,1)
+plt.scatter(X,y)
+plt.axis([-3, 3, 0, 9])
+plt.show()
+
+from sklearn.preprocessing import PolynomialFeatures
+poly = PolynomialFeatures(degree = 2, include_bias=False)
+X_poly=poly.fit_transform(X)
+
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(X_poly,y)
+
+X_new=np.linspace(-5,5,100).reshape(-1, 1)
+X_new_poly = poly.fit_transform(X_new)
+y_new = lin_reg.predict(X_new_poly)
+
+plt.scatter(X,y)
+plt.plot(X_new, y_new, c = 'r')
+plt.axis([-5,5,0,9])
+plt.show()
+
+
+
+#FIT LETS OBJECT UNDERSTAND THE DATA AND TRANSFORMS ACTUALLY CHANGES THE VALUES
